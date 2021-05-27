82121b82d271d0e1324cb55c5bb8dd6270c5a9d5
Erick Coelho <erck.coelho@gmail.com>
2021-05-27 07:48:34 -0300

    Create Project

---
+++ src/main/java/com/erickcoelho/springec/SpringecApplication.java
@@ -0,0 +1,13 @@
+package com.erickcoelho.springec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erickcoelho/springec/SpringecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erickcoelho.springec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
