25e9936448828cbe3dcb98d1bd6b52cf62b13cbe
Erick Coelho <erck.coelho@gmail.com>
2021-05-27 09:07:22 -0300

    Testing first domain Class

--- src/main/java/com/erickcoelho/springec/resources/CategoryResource.java
+++ src/main/java/com/erickcoelho/springec/resources/CategoryResource.java
@@ -1,15 +1,28 @@
 package com.erickcoelho.springec.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.erickcoelho.springec.domain.Category;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoryResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando";
+	public List<Category> listar() {
+		
+		Category cat1 = new Category(1, "Informatica");
+		Category cat2 = new Category(2, "Escritorio");
+		
+		List<Category> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
